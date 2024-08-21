10bbf864e97390485fa2b8b29c7423501c561b26
Imalkaaa <imalkadhananja28@gmail.com>
2024-08-21 13:06:36 +0530

    set mongo db correctly and change sservice

--- movies/src/main/java/dev/imalka/movies/MovieRepository.java
+++ movies/src/main/java/dev/imalka/movies/MovieRepository.java
@@ -1,6 +1,15 @@
 package dev.imalka.movies;
 
-public interface MovieRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImbdId(String imdbId);
 
 
 
